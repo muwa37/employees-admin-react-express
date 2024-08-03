2c3676ba860c2eea613b18ccac2a15b9be38134c
muwa37 <yond41m3@icloud.com>
2024-08-04 00:03:21 +0400

    Modified controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,28 @@
+const { prisma } = require('../prisma/prisma-client');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const login = async (req, res, next) => {
-  res.send('login');
+  const { email, password } = req.body;
+
+  if (!email && !password) {
+    return res.status(400).json({ message: 'please fill required fields' });
+  }
+
+  const user = await prisma.user.findFirst({ where: { email } });
+
+  const isPasswordCorrect =
+    user && (await bcrypt.compare(password, user.password));
+
+  if (isPasswordCorrect) {
+    res.status(200).json({
+      id: user.id,
+      email: user.email,
+      name: user.name,
+    });
+  } else {
+    return res.status(403).json({ message: 'wrong email or password' });
+  }
 };
 
 const register = async (req, res, next) => {
